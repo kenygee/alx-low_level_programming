dfafafcd479afe8710f3063bef4cf2b85f27dcaf
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-04 15:37:42 -0700

    commiting the hello world update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdio.h>
-
+#include <stdlb.h>
 /*
- *main - print that piece of art is useful
- *Dora Korpar, 2015-10-19
- */
+*main - print that piece of art is useful
+*
+*Dora Korpar, 2015-10-19
+*/
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
 }
