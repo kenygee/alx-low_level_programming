31fa8a1007c1a1692a7184c66c38165b014a9c45
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-04 14:41:21 -0700

    updated comitt varaible

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlb.h>
+/*
+* main - ...
+*
+* main - Prints the numbers since 0 to 9
+*
+* Return: ...
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
