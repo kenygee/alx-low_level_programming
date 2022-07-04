bbb7b988888eb76f5d5a29468181c5453574fb64
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-04 03:10:41 -0700

    comming C-files l

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ *main - print that piece of art is useful
+ *Dora Korpar, 2015-10-19
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
