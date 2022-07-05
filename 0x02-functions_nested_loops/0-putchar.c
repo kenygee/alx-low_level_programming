96d57a58eb0e9098dcb540ae9a6c0875d5a18699
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-05 15:23:12 -0700

    commiting alphabet in lower cast

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,17 @@
 * main - Print putchar
 * Return : Always return 0
 */
-int main(void)
+int main (void)
 {
 	_putchar("_");
-        _putchar("p");
-        _putchar("u");
-        _putchar("t");
-        _putchar("c");
-        _putchar("h");
-        _putchar("a");
-        _putchar("r");
-        _putchar("\n");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
-	return(0);
+return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"                                                                                                                                       
+
+/** 
+*print alphabet -> prints loweer case alphabets 
+*/
+
+void print_alphabet(void)                                                                                                                                       
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)                                                                                                                            
+	{                                                                                                                                                       	_putchar(c);
+	}
+	_putchar('\n');
+}
