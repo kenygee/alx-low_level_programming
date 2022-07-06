be3444efadc0a55ac383b041eeffc5f8a0800a01
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-06 01:21:48 -0700

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
  * print_alphabet -> print alphabet in lower cases
  *
  * Description: I play with all the alphabets and prints in lower case alphabets
  *
  * Return: Always 0
  */
+
 void print_alphabet(void)
 {
 	int j;
 
 	for (j = 'a'; j <= 'z'; j++)
 	{
-	_putchar(j);
+		_putchar(j);
 	}
 	_putchar('\n');
 }
