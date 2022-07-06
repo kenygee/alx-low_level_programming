28b2e71d1b47d0889ed184fd26276fcae4a29086
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-06 00:44:39 -0700

    Lower case alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * *print alphabet -> prints loweer case alphabets
-*/
+ * main - check the code
+ * Description: I play with all the alphabets
+ * Print alphabet -> prints loweer case alphabets
+ * Return: Always 0 (success)
+ */
 void print_alphabet(void)
-{	int j;
-	for (j = 'a'; j <= 'z'; j++)
 {
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
 	_putchar(c);
-}
+	}
 	_putchar('\n');
 }
