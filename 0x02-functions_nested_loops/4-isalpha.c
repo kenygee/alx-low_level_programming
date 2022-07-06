3572e139d5dbbed48a594e04d539fb0495bc47c6
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-06 06:11:12 -0700

    hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,10 @@
 /**
  * _isalpha -> this check for alphabet charater
  * @c: a charater to be checked
- * Description: checking for alphabet charater
- * Remark: Always return 0
+ * Return: Always return 0
  */
 
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && C <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
