dcbd7e9cc13acd0288de721041df271ec70bd025
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-06 06:59:34 -0700

    absolute value of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _abs:it compute the absolute value of integer
+ * _abs -> it compute the absolute value of integer
  * @n: delcares the integer
- * Return: integer if it fulfill every condition
+ * Return: absolute value of a number
  */
 
-int _abs(n)
+int _abs(int n)
 {
-if (n < 0)
+	if (n < 0)
 	n = -n;
 	return (n);
 }
