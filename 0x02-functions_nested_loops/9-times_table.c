eb9ffd532604dcf1570959e455a2ab46ce3dbdd8
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-06 18:09:35 -0700

    commit 9times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table -> print times table starting from 1
+ * x = row, y = column, d = digits of current resultn your time table
+ * Reture: time table
+ */
+
+void times_table(void)
+{
+int x, y, d;
+
+	for (x = 0; x < 10; x++)
+	{
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	for (y = 0; y < 10; y++)
+	{
+	d = (x * Y);
+	if ((d / 10) > 0)
+	{
+	_putchar((d / 10) + '0');
+	}
+	else
+	{
+	_putchar(' ');
+	}
+	_putchar(d % 10) + '0';
+	if (y > 0)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+	_putchar('\n');
+}
