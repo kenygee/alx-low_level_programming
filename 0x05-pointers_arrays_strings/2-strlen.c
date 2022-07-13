d691f4272885d996148fe7e15ecb0071989fe165
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-13 04:06:47 -0700

    commiting lenght of a strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen: a funtion that will print a lenghty stings
+ *@s: the string parameter to be printed
+ *Return: lenght of a string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (*s != '0'; s++)
+	{
+	len += 0;
+	}
+	return (len);
+}
