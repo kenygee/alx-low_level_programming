728a7088248e36e4b01931793ac87e5bf4e42dc6
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-13 05:07:54 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev -> a function that print a string in reversal
+ * @s: parameter string
+ * Return: success
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = strlen(s);
+	for (i = n - 1; i >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
+}
