480dbd44528640bd6047db300234980e9125aa9b
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-16 05:41:48 -0700

    hot fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,7 +5,6 @@
  * reverse_array -> a function that reverse array
  * @a: param1
  * @n: param2
- * Return: array compared
  */
 
 void reverse_array(int *a, int n)
@@ -16,5 +15,10 @@ void reverse_array(int *a, int n)
 	for (i = 0; i < n; i++)
 		p++;
 	for (k = 0; i < n / 2; k++)
-		p--;
+	{
+	aux = a[k];
+	a[k] = *p;
+	*p = aux;
+	p--;
+	}
 }
