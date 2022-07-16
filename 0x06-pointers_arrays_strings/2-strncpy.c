37eb1341bb13863ba4f891e26a39575e425362bc
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-16 04:27:31 -0700

    strncpy implimentation

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy -> a function that copies a string
+ * @dest:first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: copy of string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	_strncpy(dest, src, n);
+	return (dest);
+}
