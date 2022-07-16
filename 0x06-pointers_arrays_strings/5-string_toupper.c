19351bd11ef066bdfe17068a339d237f07bd1663
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-16 06:21:51 -0700

    hot fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,11 @@ char *string_toupper(char *x)
 {
 	int i = 0;
 
-	while (x[i]);
-{
-	if (x[i] >= 97  && x[i] <= 117)
+	while (x[i])
+	{
+	if (x[i] >= 97  && x[i] <= 122)
 		x[i] = x[i] - 32;
 		i++;
-}
+	}
 	return (x);
 }
