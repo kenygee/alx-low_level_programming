446ccff2e7f2946a24db4ce956b0ff6c5d3b9cc6
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-16 05:13:27 -0700

    array revese implimentation

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array -> a function that reverse array
+ * @a: param1
+ * @n: param2
+ * Return: array compared
+ */
+
+void reverse_array(int *a, int n)
+{
+	reverse_array(a, n);
+	int result;
+
+	result = reverse_array;
+	return (result);
+}
