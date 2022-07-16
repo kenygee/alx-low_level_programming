93de49cf8bb47f2ef00f6100c364bdfe36c73182
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-16 06:16:55 -0700

    implimenet to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,10 +3,19 @@
 
 /**
  * string_toupper -> a function that convert string to upper case
+ * @x: param
  * Return: string
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *x)
 {
-	int 
+	int i = 0;
+
+	while (x[i]);
+{
+	if (x[i] >= 97  && x[i] <= 117)
+		x[i] = x[i] - 32;
+		i++;
+}
+	return (x);
 }
