47eb01102bc2a9c4f097a7cb4fe26cc19815b8b9
Kehinde Ibiwoye <kibiwoye@gmail.com>
2022-07-16 06:01:42 -0700

    hot fix

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper -> a function that convert string to upper case
+ * Return: string
+ */
+
+char *string_toupper(char *)
+{
+	int 
+}
