b04caa3ae707ff3c8e3a57cbcc2106ea9e3f94a1
kehinde ibiwoye <kibiwoye@gmail.com>
2022-09-10 15:22:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *
+ * main - Prints the alphabeticaly
+ *   *Return: Always (Success)
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
