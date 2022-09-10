8458d57af7e33287f295ef07f8bd375b3ad3745d
kehinde ibiwoye <kibiwoye@gmail.com>
2022-09-09 20:24:16 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,8 +6,7 @@
  * Return: Always return 0
  */
 int main(void)
-int n;
-
+int n
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
@@ -19,7 +18,7 @@ else if (n == 0)
 printf("%d is zero\n", n);
 }
 else
-{	
+{
 printf("%d is positive\n", n);
 }
 return (0);
