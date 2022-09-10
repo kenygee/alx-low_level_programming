112eee0cb5904cdd9a76963b81243604ec2a6270
kehinde ibiwoye <kibiwoye@gmail.com>
2022-09-10 15:00:27 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdlb.h>
-/*
-* main - Prints the numbers since 0 to 9
-*
-* Return: Always return 0
-*/
+
+/**
+ * main - Prints the numbers since 0 to 9
+ * Return: Always return 0
+ */
 int main(void)
 {
 char n;
+
 for (n = '0'; n <= '9'; n++)
 {
 putchar(n);
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdlb.h>
-/*
-* main - ...
-*
-* main - Prints the numbers since 0 to 9
-*
-* Return: ...
-*
-* Return: Always (Success)
-*/
+
+/**
+ * main - Prints the numbers since 0 to 9
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
