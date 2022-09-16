4bc453b9d8e11128e216b95efb578897b17789b8
kehinde ibiwoye <kibiwoye@gmail.com>
2022-09-16 12:28:20 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_numbers -function that prints the numbers from 0 to 9
+ *Return: return nothing
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+	putchar(n);
+	}
+	_putchar("10"\n)
+}
