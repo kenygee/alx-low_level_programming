6354daed2af57e191d798b66157fae97d762f295
kehinde ibiwoye <kibiwoye@gmail.com>
2022-09-16 12:04:01 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isdigit - check whether a charater is a digit or not
+ *@c: tested charater c
+ *Return: 1 if c is digit otherwish 0
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+	return (0);
+	}
+}
