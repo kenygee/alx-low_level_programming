ce8204d2f11ed4743d6497fe61152de1b26e6e5a
kehinde ibiwoye <kibiwoye@gmail.com>
2022-09-18 05:44:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlb.h>
-/*
-*main - print that piece of art is useful
-*
-*Dora Korpar, 2015-10-19
+
+/**
+*main - print that piece of art is useful, Dora Korpar, 2015-10-19
+*Description: 'a C program that prints exactly the above words'
+*Return: return 1 success
 */
+
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61)
 return (1);
 }
