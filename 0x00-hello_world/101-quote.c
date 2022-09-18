266dc5ee7270c2f3dfb1fb722138f4c678e67c98
kehinde ibiwoye <kibiwoye@gmail.com>
2022-09-18 08:23:52 -0700

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-*main - print that piece of art is useful, Dora Korpar, 2015-10-19
-*Description: 'a C program that prints exactly the above words'
-*Return: return 1 success
-*/
+ *main - print that piece of art is useful, Dora Korpar, 2015-10-19
+ *Description: 'a C program that prints exactly the above words'
+ *Return: return 1 success
+ */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
