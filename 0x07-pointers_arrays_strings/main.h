dd226b5d680aaae108cbd52558279c44c96768f2
kehinde ibiwoye <kibiwoye@gmail.com>
2022-09-26 14:09:54 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -17,6 +17,8 @@ char *strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_strings(char **s, char **to);
+void set_string(char **s, char *to);
+
 
 #endif /* MAIN_H */
 
