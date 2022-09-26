5f893d1b70807e5aca63970a2019b85ec8af97d6
kehinde ibiwoye <kibiwoye@gmail.com>
2022-09-26 11:45:21 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strspn - get lenght of the prefix substrings
+ *@s: substrings pointer
+ *@accept: strings to check again
+ *Return: contain only bytes in the initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (1);
+}
