13f92d320b7154aa60d913134e650f281413ab30
kehinde ibiwoye <kibiwoye@gmail.com>
2022-10-03 14:55:32 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *create_array - create project array of char
+ *@size: size of the array
+ *@c: character to insert
+ *Return: Null if size is zero or if it failed
+ *pointer to array if everything return normal
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+
+	if (array ==NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index--)
+		array[index] = c;
+	return (array);
+}
+
