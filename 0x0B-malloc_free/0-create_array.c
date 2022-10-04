040f89a0daefd6e7d8c5ab465dae62d4e0956ab0
kehinde ibiwoye <kibiwoye@gmail.com>
2022-10-04 13:34:13 -0700

    $1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  *create_array - create project array of char
@@ -16,6 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
+
 	if (array == NULL)
 		return (NULL);
 
