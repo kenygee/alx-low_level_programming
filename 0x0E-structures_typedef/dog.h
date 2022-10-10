32ceace9bb6f4ff60dccf5eb32d8442769654a6e
kehinde ibiwoye <kibiwoye@gmail.com>
2022-10-10 16:20:52 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct dog
+ * ame: name
+ * @age: age
+ * @owner:  owne
+ * Description: This struct is for dog
+ */
+
+struct dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
+
